073e78f089bacd5caa4e6a0383d3dcb95c8c2332
Ghader <gharibyan.ghader@gmail.com>
2024-09-14 21:20:54 +0200

    lda pdp and pfi

--- frontend/src/helpers/classification/lda.js
+++ frontend/src/helpers/classification/lda.js
@@ -10,36 +10,47 @@ export default class DiscriminantAnalysis extends ClassificationModel {
             X_test: [0.8, 0.4, 1.2, 3.7, 2.6, 5.8],
         };
     }
-    async train(x, y, x_test) {
+    async train(x, y, x_test, y_test) {
         this.context = {
             lda_type: this.options.type.value,
             priors: this.options.priors.value,
             X_train: x,
             y_train: y,
             X_test: x_test,
+            y_test: y_test,
+
         };
         const script = `
         from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
         from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-        from js import X_train,y_train,X_test,lda_type,priors
+        from js import X_train,y_train,X_test,lda_type,priors,y_test
+        from sklearn.inspection import partial_dependence
+        from sklearn.inspection import permutation_importance
+
+
         if priors is not None and priors.strip():
             priors = [float(x) for x in priors.split(',')]
         else:
             priors = None
         print("priors",priors)
         if lda_type == 0:
-            da = LinearDiscriminantAnalysis(priors=priors)
+            model = LinearDiscriminantAnalysis(priors=priors)
         else:
-            da = QuadraticDiscriminantAnalysis(priors=priors)
-        da.fit(X_train, y_train)
-        y_pred = da.predict(X_test)
-        y_pred
+            model = QuadraticDiscriminantAnalysis(priors=priors)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        pdp_results = partial_dependence(model, X_train, [0])
+        fi = permutation_importance(model,X_test,y_test)
+        y_pred,pdp_results["average"],list(pdp_results["grid_values"][0]), list(fi.importances)
     `;
         try {
             const { results, error } = await asyncRun(script, this.context);
             if (results) {
-                console.log("pyodideWorker return results: ", results);
-                return Array.from(results);
+                this.predictions = Array.from(results[0]);
+                this.pdp_avergages = Array.from(results[1]);
+                this.pdp_grid = Array.from(results[2]);
+                this.importances = Array.from(results[3]);
+                return Array.from(results[0]);
             } else if (error) {
                 console.log("pyodideWorker error: ", error);
             }
@@ -48,6 +59,10 @@ export default class DiscriminantAnalysis extends ClassificationModel {
                 `Error in pyodideWorker at ${e.filename}, Line: ${e.lineno}, ${e.message}`,
             );
         }
+    }
+    async visualize(x_test, y_test, uniqueLabels, predictions, encoder) {
+        await super.visualize(x_test, y_test, uniqueLabels, predictions, encoder)
+        this.chartController.PFIBoxplot(this.id, this.importances)
     }
 
 }
